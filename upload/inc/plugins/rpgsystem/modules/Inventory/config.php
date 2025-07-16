2889e4a268e7d24f1846576020aa3d42bac179af
Anna <53200657+CaptainPaws@users.noreply.github.com>
2025-07-16 14:01:29 +0400

    Delete upload/inc/plugins/rpgsystem/modules/Inventory directory

--- upload/inc/plugins/rpgsystem/modules/Inventory/config.php
+++
@@ -1,7 +0,0 @@
-return [
-    'name' => 'inventory',
-    'title' => 'Инвентарь',
-    'description' => 'Управление предметами и слотами',
-    'version' => '1.0.0',
-    'author' => 'Ханна и Джо'
-];
